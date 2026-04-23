e394ad06250e87553aebef35b0f9ccc1012f28fe
athuljain <athuljainkj2@gmail.com>
2026-04-23 14:48:15 +0530

    add bottom tab expo icons

--- Screens/Home.jsx
+++ Screens/Home.jsx
@@ -4,12 +4,17 @@ import { View } from "react-native"
 import Welcome from "./Welcome"
 import Logout from "./Logout"
 import TodoList from "./Todo/TodoList"
+import Entypo from '@expo/vector-icons/Entypo';
 
 const Home=()=>{
     const Tab=createBottomTabNavigator()
     return(
        <Tab.Navigator>
-        <Tab.Screen name="Welcome" component={Welcome}/>
+        <Tab.Screen name="Welcome" component={Welcome}
+        options={{
+            tabBarIcon:()=><Entypo name="home" size={24} color="black" />
+        }}
+        />
         <Tab.Screen name="Logout" component={Logout}/>
         <Tab.Screen name="Todo List" component={TodoList}/>
        </Tab.Navigator>
